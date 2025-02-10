3dfb2cfde6464d7a5beacf06ecec117973cf2013
Ben <61493854+bengera@users.noreply.github.com>
2025-02-10 17:13:22 +0700

    add slider styling

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-"use strict";
-
-console.log('Testing line for JavaScript');
+for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+    e.style.setProperty('--value', e.value);
+    e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+    e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+    e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+  }
